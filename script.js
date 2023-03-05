60d4ace0e777ac155bcd5c56dfdf9a249d6362f9
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-03-05 13:48:56 +0100

    added hover effects to links and buttons

---
+++ script.js
@@ -0,0 +1,27 @@
+/* Dom selectors */
+/* const githubBtn = document.getElementById("github")
+const stackBtn = document.getElementById("stack")
+const linkedInBtn = document.getElementById("linkedIn")
+const liveDemoBtn = document.getElementById("liveDemo")
+const viewTHeCodeBtn = document.getElementById("viewTheCode")
+
+
+// Eventlisteners
+githubBtn.addEventListener('click', () => {
+   console.log("github icon clicked");
+});
+
+stackBtn.addEventListener('click', () => {
+    console.log("stack icon clicked");
+});
+linkedInBtn.addEventListener('click', () => {
+    console.log("linkedin icon clicked");
+});
+liveDemoBtn.addEventListener('click', () => {
+    console.log("live demo button clicked");
+});
+
+viewTHeCodeBtn.addEventListener('click', () => {
+    console.log("view the code button clicked");
+}); */
+
