9da1e489c176e5783dee0bf748bfc6b56940a770
Jonas Jakobson <jonas.jakobson@gmail.com>
2023-08-30 20:03:42 +0200

    created lists for conditional rendering (DRY)

---
+++ techStackList.js
@@ -0,0 +1,15 @@
+const codeList = [
+    "HTML5", "CSS3", "Javascript ES6", "Styled components", "Github", "React", "Redux"
+];
+
+const moreList = [
+   "Agile Methodology"
+];
+
+const upcomingList = [
+  "TypeScript", "Kubernetes and/or Docker"
+];
+
+const toolboxList = [
+    "Visual Studio Code", "Chrome DevTools", "Figma", "Postman", "Netlify", "MongoDB"
+];
